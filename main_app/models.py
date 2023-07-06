ddea24deb53d0a933656e9ac5d7a0ac617593ff1
bryanstark1 <bryandstark@icloud.com>
2023-07-06 12:29:54 -0400

    Created Finch model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Finch(models.Model):
+    name = models.CharField(max_length=60)
+    breed = models.CharField(max_length=75)
+    location = models.CharField(max_length=100)
+    colors = models.CharField(max_length=100)
